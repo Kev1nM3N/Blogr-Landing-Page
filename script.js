1b4a9b7b2e17b17453bf349316ca629476ae9ebc
Kevin Mendez <mediaman502@aol.com>
2024-10-31 19:47:56 -0400

    completed

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 let backgroundImgElement = document.querySelector('.background__img');
 let navMobileButton = document.querySelector('.mobileButton');
 let menuBackDropLastItem = document.querySelector('.listItemContainer__last');
+let menuBackdrop = document.getElementById('menuBackdrop');
+const landingPageIllustration = document.querySelector('.illustration')
+const laptopImage = document.querySelector('.laptopImg')
 
 navMobileButton.addEventListener('click', () => {
-    document.getElementById('menuBackdrop').classList.toggle('openMenu');
+    menuBackdrop.classList.toggle('openMenu');
+
+    menuBackdrop.classList.contains('openMenu') ? 
+    navMobileButton.src = './images/icon-close.svg' : 
+    navMobileButton.src = './images/icon-hamburger.svg';
+
 });
+
+function checkScreenSize() {
+    if (window.innerWidth <= 700) {
+      backgroundImgElement.src = './images/bg-pattern-intro-mobile.svg';
+      landingPageIllustration.src = './images/illustration-editor-mobile.svg';
+      laptopImage.src = './images/illustration-laptop-mobile.svg';
+    } else {
+      backgroundImgElement.src = './images/bg-pattern-intro-desktop.svg';
+      landingPageIllustration.src = './images/illustration-editor-desktop.svg';
+      laptopImage.src = './images/illustration-laptop-desktop.svg'
+    }
+}
+  
+checkScreenSize();
+
+window.addEventListener('resize', checkScreenSize);
