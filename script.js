8108a25f8a975c01e36bcff93b97db441d0594de
Kevin Mendez <mediaman502@aol.com>
2024-10-31 14:32:42 -0400

    working on it

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('123456');
+let backgroundImgElement = document.querySelector('.background__img');
+let navMobileButton = document.querySelector('.mobileButton');
+let menuBackDropLastItem = document.querySelector('.listItemContainer__last');
+
+navMobileButton.addEventListener('click', () => {
+    document.getElementById('menuBackdrop').classList.toggle('openMenu');
+});
